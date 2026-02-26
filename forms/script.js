9e3aa1dfdbc34e9fc6867c7589bfe6381d9187cb
Denis-alt254 <deniskipkurui2024@gmail.com>
2026-02-26 15:48:56 +0300

    displaying methods in javascript

---
+++ forms/script.js
@@ -0,0 +1,10 @@
+//document.getElementById("fname").innerHTML="This is the first name"
+
+let x = 10
+let y = 20 
+ 
+let sum = x+y
+
+
+window.alert("The sum of x and y is "+sum)
+//document.getElementById("gloria").innerHTML = sum;
